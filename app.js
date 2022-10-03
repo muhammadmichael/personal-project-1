746be0fe97757e664c72f8e508df09d0bc1624a9
muhammadmichael <michaelmaulana93@gmail.com>
2022-10-03 17:29:15 +0700

    image using multer

--- app.js
+++ app.js
@@ -19,6 +19,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/public/images/uploadedimages', express.static('public/images/uploadedimages'));
 
 // Sync DB
 const db = require("./models")
--- routes/berita.js
+++ routes/berita.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var router = express.Router();
+const fs = require("fs");
 // var bcrypt = require('bcryptjs');
 
 // const auth = require('../auth');
+const upload = require("../middleware/upload");
 const db = require('../models');
 const User = db.users;
 const Berita = db.beritas;
@@ -16,40 +18,31 @@ router.get('/tambah', function (req, res, next) {
 });
 
 // POST
-router.post('/tambah', function (req, res, next) {
-    if (req.files){
-        console.log("File was found");
-        return;
-    }else{
-        console.log("File was not found");
-        return;
-    }
-    var file = req.files.uploadedimage;
-    var img_name = file.name;
-    
-    var berita = {
-      title: req.body.title,
-      content: req.body.content,
-      image: img_name
-    }
+router.post('/tambah', upload.single('image'), function (req, res, next) {
+    console.log(req.file);
+    try {
+        console.log(req.file);    
+        if (req.file == undefined) {
+          return res.send(`You must select a file.`);
+        }
 
-    if(file.mimetype == "image/jpeg" ||file.mimetype == "image/png" ){            
-        file.mv('public/images/uploadimages/'+file.name, function(err) {               
-            if (err)
-              return res.status(500).send(err);
-                Berita.create(berita)
-                .then(
-                    res.redirect('/')
-                )
-                .catch(err => {
-                    res.redirect('/')
-                });
-                });
-    } else {
-      message = "This format is not allowed , please upload file with '.png','.jpg'";
-      res.render('index.ejs',{message: message});
+        const readFileFromPublic = req.file.filename.toString();
+        var berita = {
+            title: req.body.title,
+            content: req.body.content,
+            image: readFileFromPublic,
+        }
+
+        Berita.create(berita)
+            .then( () => {
+                // fs.writeFileSync("./public/images/tmp/", req.file.data);
+                  return res.send(`File has been uploaded.`);
+            });
+
+    } catch (error) {
+        console.log(error);
+        return res.send(`Error when trying upload images: ${error}`);
     }
-  
-  });
+});
 
 module.exports = router;
