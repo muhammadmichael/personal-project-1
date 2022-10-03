834254617ecca7a79b6621e1beb84e6dd434f0df
muhammadmichael <michaelmaulana93@gmail.com>
2022-10-03 19:58:56 +0700

    add komentar routes

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var beritaRouter = require('./routes/berita');
+var komentarRouter = require('./routes/komentar');
 
 var app = express();
 
